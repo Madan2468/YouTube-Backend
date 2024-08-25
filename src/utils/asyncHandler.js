7dd45fab2393a9f64da68f1e96655afc90de6ff9
Madan2468 <madanjha2468@gmail.com>
2024-08-25 23:55:37 +0530

    first commit

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,18 @@
+const asyncHandler = (requestHandler) => {
+ return (req, res, next) => {
+    Promise.resolve((requestHandler(req,res,next))).catch( (err) => next(err))
+  }
+}
+
+export {asyncHandler}
+
+// const asyncHandler = (fn) => async  (req,res,next) => {
+//   try {
+//     await fn(req, res, next)
+//   } catch (error) {
+//     res.status(err.code || 5000).json({
+//       success : false,
+//       message: err.message
+//     })
+//   }
+// }
